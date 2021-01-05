80961a4b20b0d754262770940db6117131b79513
Tom Igoe <tom.igoe@gmail.com>
2021-01-05 17:01:14 -0500

    added QR Code generator example

---
+++ qr-code/script.js
@@ -0,0 +1,18 @@
+function getQrCode() {
+  // set the size of the tag:
+  let tagWidth = 50;
+  let tagHeight = 50;
+  // get the text field:
+  let tagCode = document.getElementById('textField').value;
+  // start the tag request from qrserver.com:
+  let url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + tagCode;
+  // add size:
+  url += "&ampsize=" + tagWidth + "x" +tagHeight;
+  // get the image:
+  let img = document.getElementById('qrCode');
+  // fill in the properties of the image:
+  img.src = url;
+  img.alt = "a QR code of the text field";
+  img.height = tagHeight;
+  img.width = tagWidth;
+}
