5077a058757497a73316b6291c16082dd2636ccf
Tom Igoe <tom.igoe@gmail.com>
2021-01-05 11:55:55 -0500

    added fetch example

---
+++ fetch/script.js
@@ -0,0 +1,28 @@
+
+function fetchJSON() {
+  // make the HTTP/HTTPS call:
+  fetch('https://dweet.io/get/dweets/for/my-thing-name')
+    .then(response => response.json())  // convert response to JSON
+    .then(data => getResponse(JSON.stringify(data)))   // get the body of the response
+    .catch(error => getResponse(error));// if there is an error
+}
+
+function fetchText() {
+  // parameters for the HTTP/S call
+  let params = {
+    mode: 'cors', // if you need to turn off CORS, use no-cors
+    headers: {    // any HTTP headers you want can go here
+      'accept': 'application/text'
+    }
+  }
+  // make the HTTP/S call:
+  fetch('https://httpbin.org/encoding/utf8', params)
+    .then(response => response.text())  // convert response to text
+    .then(data => getResponse(data))    // get the body of the response
+    .catch(error => getResponse(error));// if there is an error
+}
+
+// function to call when you've got something to display:
+function getResponse(data) {
+  document.getElementById('result').innerHTML = data;
+}
