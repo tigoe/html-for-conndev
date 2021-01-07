fc1dfce9f7f214e7e8b7db4da39188572aedefa3
Tom Igoe <tom.igoe@gmail.com>
2021-01-07 15:11:43 -0500

    updated QR examples

--- qr-code/script.js
+++ qr-code/script.js
@@ -1,16 +1,24 @@
+/*
+  QR Code generator
+  Draws a QR code using a text string. Uses 
+  https://github.com/kazuhikoarase/qrcode-generator
+  as the QR Code generator library. It's hosted at this CDN:
+  https://unpkg.com/qrcode-generator@1.4.4/qrcode.js
+  created 7 Jan 2021
+  by Tom Igoe
+*/
+
 function getQrCode() {
-  // set the size of the tag:
-  let tagWidth = 50;
-  let tagHeight = 50;
-  // get the text field:
-  let tagCode = document.getElementById('textField').value;
-  // start the tag request from qrserver.com:
-  let url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + tagCode;
-  // get the image:
-  let img = document.getElementById('qrCode');
-  // fill in the properties of the image:
-  img.src = url;
-  img.alt = "a QR code of the text field";
-  img.height = tagHeight;
-  img.width = tagWidth;
+  // get the div element for the QR code image:
+  let qrDiv = document.getElementById('qrCode');
+  // get th text of the text field:
+  let qrText = document.getElementById('textField').value;
+  // make the QR code:
+  let qr = qrcode(0, 'L');
+  qr.addData(qrText);
+  qr.make();
+  // create an image from it:
+  let qrImg = qr.createImgTag(2, 8, "qr code of " + qrText);
+  // add it to the div:
+  qrDiv.innerHTML = qrImg;
 }
--- responsive-layout/script.js
+++ responsive-layout/script.js
@@ -1,10 +1,16 @@
 /*
   Get QR Code Script 
-  Defines Gets a QR code of the page's URL from http://goqr.me/api/doc/
+  Defines Gets a QR code of the page's URL from 
+  Uses 
+  https://github.com/kazuhikoarase/qrcode-generator
+  as the QR Code generator library. It's hosted at this CDN:
+  https://unpkg.com/qrcode-generator@1.4.4/qrcode.js
+
   and makes it the source of an image tag in the page. Shows the viewport
   size as well.
 
   created 5 Jan 2021 
+  updated 7 Jan 2021
   by Tom Igoe
 */
 
@@ -23,22 +29,15 @@ function setup() {
 
 // add a QR code of the URL when the page loads
 function getQrCode() {
-  // set the size of the tag:
-  let tagWidth = 50;
-  let tagHeight = 50;
-  // get the document URL:
-  let tagCode = document.URL;
-  // start the tag request from qrserver.com:
-  let url = 'https://api.qrserver.com/v1/create-qr-code/?data=' + tagCode;
-  // get the image and label:
-  let img = document.getElementById('qrCode');
+  // make the QR code:
+  let qr = qrcode(0, 'L');
+  qr.addData(document.URL);
+  qr.make();
+  // create an image from it:
+  let qrImg = qr.createImgTag(2, 8, "qr code of " + document.URL);
+ // get the image and label:
   let label = document.getElementById('label');
-  // fill in the properties of the image and label:
-  img.src = url;
-  img.alt = "a QR code of the document URL";
-  img.height = tagHeight;
-  img.width = tagWidth;
-  label.innerHTML = document.URL;
+  label.innerHTML = qrImg + '<br>' + document.URL;
 }
 
 // on page load, call the QR code function:
