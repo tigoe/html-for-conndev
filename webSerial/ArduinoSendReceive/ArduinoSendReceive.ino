5b9313294bc073c1522879328b903f949d6c636a
Tom Igoe <tom.igoe@gmail.com>
2022-01-28 16:57:35 -0500

    Update ArduinoSendReceive.ino

--- webSerial/ArduinoSendReceive/ArduinoSendReceive.ino
+++ webSerial/ArduinoSendReceive/ArduinoSendReceive.ino
@@ -2,18 +2,23 @@ long lastSecond = 0;
 
 void setup() {
   Serial.begin(9600);
+  // set a parsing timeout of 10ms
+  // (used by Serial.readString, parseInt, etc):
   Serial.setTimeout(10);
 }
 
 void loop() {
+  // echo back whatever you receive:
   if (Serial.available()) {
     String incoming = Serial.readString();
     Serial.println(incoming);
   }
 
+// send the seconds as a JSON string:
   if (millis() - lastSecond > 1000) {
-    Serial.print("Seconds: ");
-    Serial.println(millis() / 1000);
+    Serial.print("{\"secs\": ");
+    Serial.print(millis() / 1000);
+    Serial.println("}");
     lastSecond = millis();
   }
 }
