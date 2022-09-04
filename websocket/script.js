38177ac69c449c4ffa9f8a27249ef85070e3d029
Tom Igoe <tom.igoe@gmail.com>
2022-09-04 11:24:58 -0400

    update websocket client

--- websocket/script.js
+++ websocket/script.js
@@ -2,13 +2,13 @@
   WebSocket connection Script
   Uses standard W3C WebSocket API, not socket.io API
   Connects to websocket.org's test server at 
-  https://echo.websocket.events
+  wss://echo-websocket.hoppscotch.io
 
   created 7 Jan 2021
-  modified 28 Jan 2022
+  modified 4 Sep 2022
   by Tom Igoe
 */
-const serverURL = 'wss://echo.websocket.events/';
+const serverURL = 'wss://echo-websocket.hoppscotch.io';
 let socket;
 // variables for the DOM elements:
 let incomingSpan;
